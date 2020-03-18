362d8137b8a74883b74866bd4c4531ae74e74613
seb_fixe <sebastien.lerda@epitech.eu>
2020-03-18 11:48:30 +0100

    i = 1 et plus 0

--- script/ex_02.js
+++ script/ex_02.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var el = document.querySelector('footer').querySelector('div');
-    var i = 0;
+    var i = 1;
     el.addEventListener("click", function()
         {
             el.innerText = i++;
