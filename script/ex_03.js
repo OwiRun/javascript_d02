f9facb72616cab97b7d17792fbd9ee96f3040c11
seb_fixe <sebastien.lerda@epitech.eu>
2020-03-18 15:25:10 +0100

    amelio lisibilité

--- script/ex_03.js
+++ script/ex_03.js
@@ -1,26 +1,29 @@
 window.onload = function ()
 {
     var el = document.querySelector('footer').querySelector('div'); 
-    el.addEventListener("click", userName = function()
-    {    
-        myPrompt = prompt("Entrez votre nom !");
-        while(myPrompt == "")
-        {
-            myPrompt;
+    el.addEventListener
+    (
+        "click", userName = function()
+        {    
             myPrompt = prompt("Entrez votre nom !");
-        }
-        if(myPrompt != null)
-        {
-            el.innerText = myPrompt;
-            if (confirm("Etes vous sûr que '" + myPrompt +"' est votre nom ?")) 
+            while(myPrompt == "")
+            {
+                myPrompt;
+                myPrompt = prompt("Entrez votre nom !");
+            }
+            if(myPrompt != null)
             {
                 el.innerText = myPrompt;
-                alert("Bonjour " + myPrompt +"!");
-            } 
-            else 
-            {
-                return userName(); 
+                if (confirm("Etes vous sûr que '" + myPrompt +"' est votre nom ?")) 
+                {
+                    el.innerText = myPrompt;
+                    alert("Bonjour " + myPrompt +"!");
+                } 
+                else 
+                {
+                    return userName(); 
+                }
             }
         }
-    })
+    )
 }
